9eceeb6278e60a2eb53bebaa9b44f8987f6b3e84
Hanzlah <hanzlahjavaid849@example.com>
2024-10-03 17:05:57 +0500

    half-way

--- src/app/(shop)/order/checkout/page.tsx
+++ src/app/(shop)/order/checkout/page.tsx
@@ -4,6 +4,7 @@ import { CheckoutProducts } from "./_components/checkout-products";
 
 export default async function page() {
   const { user } = await validateRequest();
+
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-8 md:py-12">
       <div className="grid grid-cols-1 gap-5 pt-12 lg:grid-cols-[60%_40%] xl:grid-cols-[65%_35%]">
